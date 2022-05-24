4eb1240ad7935229b1b4f8398529c43a701f3d8c
JBLardet <jeanbaptiste.lardet2022@campus-eni.fr>
2022-05-24 17:44:47 +0200

    Travail non-fini JB + correction type variables sur Utilisateur et mise en page de Retrait et ArticleVendu

--- src/main/java/fr/eni/java/projet/bo/Retrait.java
+++ src/main/java/fr/eni/java/projet/bo/Retrait.java
@@ -4,35 +4,43 @@ public class Retrait {
 	private String rue;
 	private int code_postal;
 	private String ville;
+
 	public String getRue() {
 		return this.rue;
 	}
+
 	public void setRue(String rue) {
 		this.rue = rue;
 	}
+
 	public int getCode_postal() {
 		return this.code_postal;
 	}
+
 	public void setCode_postal(int code_postal) {
 		this.code_postal = code_postal;
 	}
+
 	public String getVille() {
 		return this.ville;
 	}
+
 	public void setVille(String ville) {
 		this.ville = ville;
 	}
+
 	public Retrait() {
 	}
+
 	public Retrait(String rue, int code_postal, String ville) {
 		this.rue = rue;
 		this.code_postal = code_postal;
 		this.ville = ville;
 	}
+
 	@Override
 	public String toString() {
-		return "Le retrait se fera à l'adresse suivante:%n [rue=" + this.rue + ", code_postal=" + this.code_postal + ", ville=" + this.ville + "]";
+		return "Le retrait [rue=" + this.rue + ", code_postal=" + this.code_postal + ", ville=" + this.ville + "]";
 	}
-	
-	
+
 }
---
+++ src/main/java/fr/eni/java/projet/servlets/ServletInscriptionUtilisateur.java
@@ -0,0 +1,52 @@
+package fr.eni.java.projet.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletInscriptionUtilisateur
+ */
+@WebServlet("/ServletInscriptionUtilisateur")
+public class ServletInscriptionUtilisateur extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/Inscription.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String pseudo = (String) request.getParameter("pseudo");
+		String nom = (String) request.getParameter("nom");
+		String prenom = (String) request.getParameter("prenom");
+		String email = (String) request.getParameter("email");
+		String telephone = (String) request.getParameter("tel");
+		String rue = (String) request.getParameter("rue");
+		String codePostal = (String) request.getParameter("cp");
+		String ville = (String) request.getParameter("ville");
+		String motDePasse = (String) request.getParameter("mdp");
+		doGet(request, response);
+	}
+
+}
