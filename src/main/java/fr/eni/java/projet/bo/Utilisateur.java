cf285ad6d439fba5f1d073e7f46f3a0c6ed95c0d
Sofiacks <sofia.elattaria2022@campus-eni.fr>
2022-05-24 18:34:19 +0200

    util

--- src/main/java/fr/eni/java/projet/bo/Utilisateur.java
+++ src/main/java/fr/eni/java/projet/bo/Utilisateur.java
@@ -7,9 +7,9 @@ public class Utilisateur {
 	private String nom;
 	private String prenom;
 	private String email;
-	private int telephone;
+	private String telephone;
 	private String rue;
-	private int codePostal;
+	private String codePostal;
 	private String ville;
 	private String motDePasse;
 	private int credit;
